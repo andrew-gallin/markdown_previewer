d2b6e4f66cafca218cd46eedc4c5a7676d0f1cf3
Andrew Gallin <andrewgallin@gmail.com>
2018-09-13 06:49:56 -0400

    added state based styling

--- app/components/App.js
+++ app/components/App.js
@@ -2,28 +2,35 @@ import React from 'react'
 import marked from 'marked';
 import css from './css/main.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { faExpandArrowsAlt } from '@fortawesome/free-solid-svg-icons'
 import { faFreeCodeCamp } from '@fortawesome/free-brands-svg-icons'
+
+const Modal_Button = (props) => {
+  return (
+    <div className = 'button'><FontAwesomeIcon icon={faExpandArrowsAlt}/></div>
+  )
+}
 
 const Toolbar = (props) => {
   return (
     <div className = 'toolbar' id = {props.id}>
       {
-        <div className = 'tbElements'><FontAwesomeIcon icon={faFreeCodeCamp}/><p className='title'>{props.title}</p></div>
+        <div className = 'tbElements'><FontAwesomeIcon icon={faFreeCodeCamp}/>
+          <p className='title'>{props.title}</p><Modal_Button /></div>
       }
     </div>
   )
 }
-
 
 class Markdown_Modal extends React.Component {
   constructor(props) {
     	super(props)
   	}
   render() {
-    return (<div className = 'md_modal'>
+    return (<div className = {this.props.class}>
     	<Toolbar title='Editor' />
-      	<textarea id = 'editor' type ='text' value = {this.props.value} onChange={this.props.handleChange}/>
+      	<textarea id = 'editor' type ='text' value = {this.props.value}
+      	  onChange={this.props.handleChange}/>
     </div>)
   }
 }
@@ -33,11 +40,11 @@ class Preview_Modal extends React.Component {
     	super(props)
   	}
   createMarkup(markup) {
-    return {__html: markup};
+    return { __html: markup }
   }
   render() {
   	let text = this.props.text
-    return (<div className = 'preview_modal'>
+    return (<div className = {this.props.class}>
     	<Toolbar title='Preview' id = 'preview'/>
       	<p dangerouslySetInnerHTML = {this.createMarkup(text)}></p>
     </div>)
@@ -46,12 +53,16 @@ class Preview_Modal extends React.Component {
 
 class Application extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       text: 'Enter text here',
-      marked_text: 'Display'
+      marked_text: 'Display',
+      md_max: true,
+      preview_max: false
     }
     this.handleChange = this.handleChange.bind(this)
+    this.md_max = this.md_max.bind(this)
+    this.preview_max = this.preview_max.bind(this)
   }
   handleChange(event) {
     this.setState({
@@ -59,10 +70,32 @@ class Application extends React.Component {
       marked_text: marked(event.target.value)
     })
   }
+  md_max() {
+    this.setState({
+      md_max: true
+    })
+  }
+  preview_max() {
+    this.setState({
+      preview_max: true
+    })
+  }
   render() {
+  	if (this.state.md_max) {
+  		    return (<div>
+        <Markdown_Modal class = 'modal modal_max' value={this.state.text}
+        	handleChange={this.handleChange} />
+      </div>)
+  	}
+  	if (this.state.preview_max) {
+  		    return (<div>
+        <Preview_Modal class = 'modal modal_max' text = {this.state.marked_text}/>
+      </div>)
+  	}
     return (<div>
-      <Markdown_Modal value={this.state.text} handleChange={this.handleChange} />
-      <Preview_Modal text = {this.state.marked_text}/>
+      <Markdown_Modal class= 'modal md_modal' value={this.state.text}
+      	handleChange={this.handleChange} />
+      <Preview_Modal class = 'modal preview_modal' text = {this.state.marked_text}/>
     </div>)
   }
 }
