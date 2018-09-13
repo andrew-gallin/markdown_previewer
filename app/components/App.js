585e502254abbca2c7b828cbeab974152a0420be
Andrew Gallin <andrewgallin@gmail.com>
2018-09-13 13:53:57 -0400

    added click functionality and icons

--- app/components/App.js
+++ app/components/App.js
@@ -3,12 +3,19 @@ import marked from 'marked';
 import css from './css/main.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faExpandArrowsAlt } from '@fortawesome/free-solid-svg-icons'
+import { faCompress } from '@fortawesome/free-solid-svg-icons'
 import { faFreeCodeCamp } from '@fortawesome/free-brands-svg-icons'
 
 const Modal_Button = (props) => {
+  if (props.isMax) {
+	  return (
+	    <div className = 'button' onClick={props.max}><FontAwesomeIcon
+	    icon={faCompress}/></div>
+	  )
+  }
   return (
-    <div className = 'button'><FontAwesomeIcon icon={faExpandArrowsAlt}/></div>
-  )
+    <div className = 'button' onClick={props.max}><FontAwesomeIcon icon={faExpandArrowsAlt}/></div>
+	  )
 }
 
 const Toolbar = (props) => {
@@ -16,7 +23,8 @@ const Toolbar = (props) => {
     <div className = 'toolbar' id = {props.id}>
       {
         <div className = 'tbElements'><FontAwesomeIcon icon={faFreeCodeCamp}/>
-          <p className='title'>{props.title}</p><Modal_Button /></div>
+          <p className='title'>{props.title}</p>
+          <Modal_Button max={props.max} isMax = {props.isMax}/></div>
       }
     </div>
   )
@@ -28,7 +36,7 @@ class Markdown_Modal extends React.Component {
   	}
   render() {
     return (<div className = {this.props.class}>
-    	<Toolbar title='Editor' />
+    	<Toolbar title='Editor' max={this.props.max} isMax = {this.props.isMax}/>
       	<textarea id = 'editor' type ='text' value = {this.props.value}
       	  onChange={this.props.handleChange}/>
     </div>)
@@ -45,7 +53,8 @@ class Preview_Modal extends React.Component {
   render() {
   	let text = this.props.text
     return (<div className = {this.props.class}>
-    	<Toolbar title='Preview' id = 'preview'/>
+    	<Toolbar title='Preview' id = 'preview' max={this.props.max}
+    	  isMax = {this.props.isMax}/>
       	<p dangerouslySetInnerHTML = {this.createMarkup(text)}></p>
     </div>)
   }
@@ -57,12 +66,12 @@ class Application extends React.Component {
     this.state = {
       text: 'Enter text here',
       marked_text: 'Display',
-      md_max: true,
+      md_max: false,
       preview_max: false
     }
     this.handleChange = this.handleChange.bind(this)
-    this.md_max = this.md_max.bind(this)
-    this.preview_max = this.preview_max.bind(this)
+    this.mdMax = this.mdMax.bind(this)
+    this.previewMax = this.previewMax.bind(this)
   }
   handleChange(event) {
     this.setState({
@@ -70,32 +79,48 @@ class Application extends React.Component {
       marked_text: marked(event.target.value)
     })
   }
-  md_max() {
-    this.setState({
-      md_max: true
-    })
+  mdMax(event) {
+  	if (this.state.md_max) {
+	    this.setState({
+	      md_max: false
+	    })
+	  }
+    else {
+	 	this.setState({
+	      md_max: true
+	    })
+	 }
   }
-  preview_max() {
-    this.setState({
-      preview_max: true
-    })
+  previewMax(event) {
+    if (this.state.preview_max) {
+	    this.setState({
+	      preview_max: false
+	    })
+	  }
+    else {
+	 	this.setState({
+	      preview_max: true
+	    })
+	 }
   }
   render() {
   	if (this.state.md_max) {
-  		    return (<div>
+  		    return (<div className='wrapper'>
         <Markdown_Modal class = 'modal modal_max' value={this.state.text}
-        	handleChange={this.handleChange} />
+        	handleChange={this.handleChange} max = {this.mdMax} isMax= {this.state.md_max}/>
       </div>)
   	}
   	if (this.state.preview_max) {
-  		    return (<div>
-        <Preview_Modal class = 'modal modal_max' text = {this.state.marked_text}/>
+  		    return (<div className='wrapper'>
+        <Preview_Modal class = 'modal modal_max' text = {this.state.marked_text}
+          max = {this.previewMax} isMax= {this.state.preview_max}/>
       </div>)
   	}
-    return (<div>
+    return (<div className='wrapper'>
       <Markdown_Modal class= 'modal md_modal' value={this.state.text}
-      	handleChange={this.handleChange} />
-      <Preview_Modal class = 'modal preview_modal' text = {this.state.marked_text}/>
+      	handleChange={this.handleChange} max={this.mdMax} isMax= {this.state.preview_max}/>
+      <Preview_Modal class = 'modal preview_modal' text = {this.state.marked_text}
+        max = {this.previewMax} isMax= {this.state.md_max}/>
     </div>)
   }
 }
