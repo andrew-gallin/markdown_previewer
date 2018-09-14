eee81d63cc13fb9b0dafcc0bebf26950c21110a2
Andrew Gallin <andrewgallin@gmail.com>
2018-09-13 21:10:23 -0400

    styling updates

--- app/components/App.js
+++ app/components/App.js
@@ -6,6 +6,58 @@ import { faExpandArrowsAlt } from '@fortawesome/free-solid-svg-icons'
 import { faCompress } from '@fortawesome/free-solid-svg-icons'
 import { faFreeCodeCamp } from '@fortawesome/free-brands-svg-icons'
 
+const placeholder = 
+`# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+  
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+  
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.com), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | ------------- 
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbererd lists too.
+1. Use just 1s if you want! 
+1. But the list goes on...
+- Even if you use dashes or asterisks.
+* And last but not least, let's not forget embedded images:
+
+![React Logo w/ Text](https://goo.gl/Umyytc)
+`
+
+marked.setOptions({
+  breaks: true
+})
+
 const Modal_Button = (props) => {
   if (props.isMax) {
 	  return (
@@ -53,9 +105,9 @@ class Preview_Modal extends React.Component {
   render() {
   	let text = this.props.text
     return (<div className = {this.props.class}>
-    	<Toolbar title='Preview' id = 'preview' max={this.props.max}
+    	<Toolbar title='Preview'  max={this.props.max}
     	  isMax = {this.props.isMax}/>
-      	<p dangerouslySetInnerHTML = {this.createMarkup(text)}></p>
+      	<p id = 'preview' dangerouslySetInnerHTML = {this.createMarkup(text)}></p>
     </div>)
   }
 }
@@ -64,8 +116,8 @@ class Application extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      text: 'Enter text here',
-      marked_text: 'Display',
+      text: placeholder,
+      marked_text: marked(placeholder),
       md_max: false,
       preview_max: false
     }
@@ -124,5 +176,7 @@ class Application extends React.Component {
     </div>)
   }
 }
+
+
 
 export default Application;
--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
+import marked from 'marked'
 
 ReactDOM.render(
 	<App />, 
